7bc7a37d593e52db62660a3749e8eafa066f53f2
Vasiliy Faronov <vfaronov@gmail.com>
2019-08-29 14:34:24 +0300

    Serial numbers of posts

--- store/post.go
+++ store/post.go
@@ -6,11 +6,14 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Post struct {
 	ID     primitive.ObjectID `bson:"_id,omitempty"`
 	RoomID primitive.ObjectID `bson:"room_id"`
+	Serial uint32
 	Author string
 	Time   time.Time
 	Text   string
@@ -20,15 +23,31 @@ func (db *DB) CreatePost(ctx context.Context, post *Post) error {
 	post.ID = primitive.NilObjectID
 	post.Time = time.Now()
 
-	res1, err := db.rooms.UpdateOne(ctx, bson.M{"_id": post.RoomID},
-		bson.M{"$set": bson.M{"updated": post.Time}})
-	if err != nil {
+	// Update the room to ensure that it exists, bump its update timestamp,
+	// and acquire the serial number for this post. Two posts will never get
+	// the same serial number because $inc on one master is atomic.
+	res1 := db.rooms.FindOneAndUpdate(ctx,
+		bson.M{"_id": post.RoomID},
+		bson.M{
+			"$set": bson.M{"updated": post.Time},
+			"$inc": bson.M{"serial": 1},
+		},
+		options.FindOneAndUpdate().SetReturnDocument(options.After),
+	)
+	var room struct {
+		Serial uint32
+	}
+	err := res1.Decode(&room)
+	switch {
+	case err == mongo.ErrNoDocuments:
+		return NotFound
+	case err != nil:
 		return err
 	}
-	if res1.ModifiedCount < 1 {
-		return NotFound
-	}
+	post.Serial = room.Serial
 
+	// Now insert the actual post. If this fails, we end up with slightly
+	// inconsistent room data, which is tolerable.
 	res, err := db.posts.InsertOne(ctx, post)
 	if err != nil {
 		return err
